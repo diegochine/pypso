c98fd0f557a059e8a5c2c0fcde041819b8bb4e99
Diego Chinellato <chine.diego@gmail.com>
2021-06-23 22:55:37 +0200

    CH score, minor improvements

--- clustering.py
+++ clustering.py
@@ -1,13 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
-from sklearn.datasets import make_blobs
-from sklearn.metrics import calinski_harabasz_score
 from pso import GBestPSO
 
 
 def assign_clusters(X, clusters):
-    distance_matrix = np.empty((n_samples, k))
+    distance_matrix = np.empty((X.shape[0], n_clusters))
     for i, point in enumerate(X):
         cp = np.reshape(point, (1, -1)).copy()
         distance_matrix[i, :] = np.sum((clusters - cp) ** 2, axis=1)
@@ -15,39 +13,31 @@ def assign_clusters(X, clusters):
     return labels
 
 
+def calinski_harabasz_score(X, labels):
+    # see https://www.researchgate.net/publication/233096619_A_Dendrite_Method_for_Cluster_Analysis
+    # and https://www.journaljamcs.com/index.php/JAMCS/article/view/24229
+    intra_dispersion = 0.
+    extra_dispersion = 0.
+    mean_data = np.mean(X, axis=0)
+    for k in range(n_clusters):
+        cluster = X[labels == k]
+        if cluster.shape != (0, 2):
+            mean_cluster = np.mean(cluster, axis=0)
+            extra_dispersion += cluster.shape[0] * np.sum((mean_cluster - mean_data) ** 2)
+            intra_dispersion += np.sum((cluster - mean_cluster) ** 2)
+    if extra_dispersion == 0:
+        return 1.
+    else:
+        n_samples = X.shape[0]
+        n_labels = np.unique(labels).size
+        return extra_dispersion * (n_samples - n_labels) / (intra_dispersion * (n_labels - 1))
+
+
 def f(pos):
     """ Overhead function to perform clustering using the swarm"""
     labels = assign_clusters(X, pos)
-    return - calinski_harabasz_score(X, labels)
-
-
-def plot_clusters(X, y, points, labels_prefix='cluster', points_name='centroids', ):
-    """
-    Plot a two dimensional projection of an array of labelled points
-    X:      array with at least two columns
-    y:      vector of labels, length as number of rows in X
-    dim:    the two columns to project, inside range of X columns, e.g. (0,1)
-    points: additional points to plot as 'stars'
-    labels_prefix: prefix to the labels for the legend ['cluster']
-    points_name:   legend name for the additional points ['centroids']
-    """
-    # plot the labelled (colored) dataset and the points
-    labels = np.unique(y)
-    for i in range(len(labels)):
-        plt.scatter(X[y == labels[i], 0],
-                    X[y == labels[i], 1],
-                    s=10,
-                    marker='s', edgecolors='k',
-                    label=labels_prefix + str(labels[i]))
-    plt.scatter(points[:, 0],
-                points[:, 1],
-                s=50,
-                marker='*',
-                # c=[points_color],
-                label=points_name)
-    plt.legend()
-    plt.grid()
-    plt.show()
+    penalty = 0 if np.unique(labels).size == n_clusters else 1000
+    return - calinski_harabasz_score(X, labels) + penalty
 
 
 def animate(cluster_info, points, clusters_plots, centroids_plots, iter_text):
@@ -82,30 +72,26 @@ def animate_clusters(swarm, points):
         centroid_plot, = ax.plot([], [], ls='None', marker='*', ms=15,
                                  markeredgecolor='k', markerfacecolor=colors[k])
         centroids_plots.append(centroid_plot)
-    iter_text = ax.annotate('', xy=(0.01, 0.01), xycoords='axes fraction')
+    iter_text = ax.annotate('', xy=(0.01, 0.01), xycoords='axes fraction', fontsize='x-large')
     pad = 0.1
     ax.set_xlim(points[:, 0].min() - pad, points[:, 0].max() + pad)
     ax.set_ylim(points[:, 1].min() - pad, points[:, 1].max() + pad)
+    ax.set_title('PSO clustering animation')
     anim = animation.FuncAnimation(fig, animate, frames=generator,
                                    fargs=(points, clusters_plots, centroids_plots, iter_text),
                                    repeat=True, interval=500)
     plt.legend()
-    plt.grid()
     plt.show()
     return anim
 
 
 if __name__ == '__main__':
-    samples_per_cluster = [300, 200, 500, 300, 300]
-    n_samples = np.sum(samples_per_cluster)
-    n_features = 2
-    centers = 5
-    X, y = make_blobs(n_samples=n_samples, n_features=n_features, centers=centers,
-                      cluster_std=[1.0, 0.3, 2.5, 1.5, 0.5], random_state=1)
+    X = np.load('data/cluster_data.npy')
+    y = np.load('data/cluster_labels.npy')
     bounds = (np.min(X, axis=0), np.max(X, axis=0))
-    hyparams = {'c1': 2.1, 'c2': 2.1, 'fully_informed': False}
-    for k in range(5, 7):
-        swarm = GBestPSO(150, (k, n_features), bounds=bounds, verbose=True)
-        c, p = swarm.minimize(f, iters=20)
-        print(f'final cost: {c}')
-        ani = animate_clusters(swarm, X)
+    hyparams = {'c1': 2.1, 'c2': 2.1}
+    n_clusters = 5
+    swarm = GBestPSO(100, (n_clusters, 2), bounds=bounds, hyparams=hyparams, verbose=True)
+    c, p = swarm.minimize(f, iters=20)
+    print(f'final cost: {c}')
+    ani = animate_clusters(swarm, X)
